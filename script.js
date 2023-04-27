76d5975c18ea0e74f6958aea5ce8d609243a05ed
Vitalii Povierienov | Front-end developer <73063388+vitpoverenov@users.noreply.github.com>
2023-04-27 11:09:19 +0200

    Add 1 column
    In plans add 2 column.

---
+++ script.js
@@ -0,0 +1,39 @@
+let button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+  let result = 0;
+
+  let a1 = document.getElementById("q1").value;
+  if (a1 === "2") {
+    result++;
+  }
+
+  let a2 = document.getElementById("q2").value;
+  if (a2 === "5") {
+    result++;
+  }
+
+  let a3 = document.getElementById("q3").value;
+  if (a3 === "7") {
+    result++;
+  }
+
+  let a4 = document.getElementById("q4").value;
+  if (a4 === "12") {
+    result++;
+  }
+
+  let a5 = document.getElementById("q5").value;
+  if (a5 === "9") {
+    result++;
+  }
+
+  let a6 = document.getElementById("q6").value;
+  if (a6 === "8") {
+    result++;
+  }
+
+  alert("Правильных ответов:" +result);
+}
